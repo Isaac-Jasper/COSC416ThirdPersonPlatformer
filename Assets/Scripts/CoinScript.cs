497578e501592daca6c58af5de7d2dc9ffea201a
Isaac Jasper <60370374+Isaac-Jasper@users.noreply.github.com>
2025-03-02 18:08:05 -0800

    Score implemented

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoinScript : MonoBehaviour {
+    [SerializeField]
+    public UnityEvent CoinCollected = new UnityEvent();
     [SerializeField]
     private float spinSpeed;
 
     void Update() {
         transform.Rotate(spinSpeed * Time.deltaTime * Vector3.up);
     }
-    
+
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
-            //increment score
+            CoinCollected?.Invoke();
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -1,16 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class ScoreController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    [SerializeField]
+    private TMP_Text scoreText;
+    private int score;
+
+    void Start() {
+        CoinScript[] coins = FindObjectsByType<CoinScript>(FindObjectsSortMode.None);
+        foreach (CoinScript coin in coins) {
+            Debug.Log("" + coin.name);
+            coin.CoinCollected.AddListener(IncrementScore);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void IncrementScore() {
+        score++;
+        scoreText.text = $"Score: {score:00}";
     }
 }
